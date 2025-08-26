3090390cd95830c7ce64275ccc139049f3f094b1
dimacia666 <dimahotab666@gmail.com>
2025-08-26 16:56:57 +0300

    sumary

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const btn4 = document.querySelector(".btn4");
+const btn5 = document.querySelector(".btn5");
+ 
+btn.addEventListener("click", () => { 
+    btn.textContent = "Попалась)";
+});
+btn1.addEventListener("click", () => { 
+    btn1.textContent = "Люблю";
+});
+btn2.addEventListener("click", () => { 
+    btn2.textContent = "Тебя";
+});
+btn3.addEventListener("click", () => { 
+    btn3.textContent = "Жопка)";
+});
+btn4.addEventListener("click", () => { 
+    btn4.textContent = "Моя";
+});
+btn5.addEventListener("click", () => { 
+    btn5.textContent = "Любимая!!!";
+});
